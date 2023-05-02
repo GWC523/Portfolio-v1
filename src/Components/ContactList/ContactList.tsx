27e3087722b8d0e2f06e23b939c4e1f0720c636b
GWC523 <gwynethchiu15@gmail.com>
2023-05-03 01:09:36 +0800

    added links to contact list

--- src/Components/ContactList/ContactList.tsx
+++ src/Components/ContactList/ContactList.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import { faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import Modal from '../../Components/Modal/Modal';
 
 /* CSS */
 import './ContactList.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 function ContactList() {
+  const [seeEmail, setSeeEmail] = useState(false);
+  const copyRef = useRef<HTMLInputElement>(null);
   let github_link = 'https://github.com/GWC523';
   let LinkedIn_link = 'https://www.linkedin.com/in/gwyneth-chiu/';
+
+  const handleCopyToClipboard = (str: any) => {
+    if(copyRef.current) {
+      copyRef.current.select();
+      navigator.clipboard.writeText(str)
+    }
+  }
+
+    const copyToClipboardInput = () => {
+    return (
+      <div className='copy-modal-container'>
+      <div className='copy-text'>
+        <input type='text' className='copy-text-input' ref={copyRef} value='gwchiu@up.edu.ph' />
+        <button onClick={() => handleCopyToClipboard('gwchiu@up.edu.ph')}>
+          <FontAwesomeIcon
+            icon={'clone'}
+            title={'copy to clipboard'}
+            aria-hidden='true'
+            className='copy-icon'
+          />
+        </button>
+      </div>
+      </div>
+    )
+  }
 
   return (
     <div className='contact-list-cont'>
@@ -18,6 +46,7 @@ function ContactList() {
                 title={'email'}
                 aria-hidden='true'
                 className='email-icon'
+                onClick={() => setSeeEmail(true)}
                 />  
             </li>
             <li>
@@ -26,6 +55,7 @@ function ContactList() {
                 title={'github'}
                 aria-hidden='true'
                 className='github-icon'
+                onClick = {() => { window.open(github_link, '_blank', 'noreferrer') }}
                 />  
             </li>
             <li>
@@ -38,6 +68,13 @@ function ContactList() {
                 />  
             </li>
         </ul>
+         <Modal
+            title='Contact Me'
+            subTitle='via email'
+            showModal={seeEmail}
+            setShowModal={setSeeEmail}
+            children={copyToClipboardInput()}
+        />
     </div>
   )
 }
